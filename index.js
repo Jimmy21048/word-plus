162c24e5c23468e5710efe0c7a94c603befc9908
Jimmy Rubia <rubiajimmy3@gmail.com>
2025-02-21 12:26:37 +0300

    integrated gemini ai. version 1.0 complete

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-// console.log(window.getSelection().toString())
 const text = document.getElementById('text')
 let search
 
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs)  => {
     chrome.tabs.sendMessage(tabs[0].id, { action: "getSelectedText" }, (response) => {
         if(response && response.text) {
-            search = response.text
-            text.innerHTML = response.text
+            let query = response.text
+
+            const requestBody = {
+                contents: [{ parts: [{ 
+                    text: `Give the simplest meaning of the following word/phrase. 
+                    keep the explanation simple, if possible just one sentence. 
+                    also add an example in a sentence. 
+                    The meaning and example should be on different lines and enclosed within <p>Meaning: ...</p> and <p>Example: ...</p> tags. exclude the '*'.
+                    The word/phrase is: ${query}` }] }],
+              };
+
+            fetch(`https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=AIzaSyAfWR7WDUlcTcD1mjBwTRlKLV1jIfV19WQ`, {
+                method : "POST",
+                headers : {
+                    headers: { "Content-Type": "application/json" },
+                },
+                body: JSON.stringify(requestBody)
+            }).then(res => res.json()).then(data => {
+                search = data.candidates[0].content.parts[0].text
+                text.innerHTML = search
+            }).catch(err => {
+                text.innerHTML = "Sorry, an error occurred"
+            })
+            
         } else {
-            text.innerHTML = "No text selected"
+            text.innerHTML = "Error!, please close and reopen chrome"
         }
     })
 })
-
-// text.innerHTML = "Jimmy rubia"
 
--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,3 @@
-// console.log(window.getSelection().toString())
-// const text = document.getElementById('text').innerHTML
-// console.log("This is the text")
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log(request.action)
     if (request.action === "getSelectedText") {
